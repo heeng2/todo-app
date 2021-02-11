e278051ebcb0db8c100d425c956513c37635de5b
dorang <heeng2@naver.com>
2021-02-11 20:19:07 +0900

    11.5 todos가 딱 바뀌는 요소만 리렌더링되도록 최적화

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
+function createBulkTodos(){
+    const array = [];
+    for (let i =1; i<=2500; i++){
+        array.push({
+            id:i,
+            text: `할 일 ${i}`,
+            checked: false
+        });
+    }
+    return array;
+}
+
 const App = () => {
-    const [todos, setTodos] = useState([
+    const [todos, setTodos] =
+        useState(createBulkTodos);
+
+        /*
+        useState([
         {
             id:1,
             text: '리액트의 기초 알아보기',
@@ -20,7 +36,7 @@ const App = () => {
             text: '일정 관리 앱 만들어 보기',
             checked: false,
         },
-    ]);
+    ]);*/
 
     const nextId = useRef(4);
 
@@ -31,28 +47,28 @@ const App = () => {
                 text,
                 checked : false
             };
-            setTodos(todos.concat(todo));
+            setTodos(todos=> todos.concat(todo));
             nextId.current +=1;
         },
-        [todos]
+        []
     )
 
     const onRemove = useCallback(
         id=>{
-            setTodos(todos.filter(todo=>todo.id!==id));
+            setTodos(todos=>todos.filter(todo=>todo.id!==id));
         },
-        [todos],
+        [],
     )
 
     const onToggle = useCallback(
         id=>{
-            setTodos(
+            setTodos(todos=>
                 todos.map(todo=>
                     todo.id === id? {...todo, checked: !todo.checked}:todo,
                 )
             )
         },
-        [todos],
+        [],
     );
 
     return (
